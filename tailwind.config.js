bf390b4733041cda344fbbad0f18150595c9ff6b
Mmesoma-Constance <mmesomanwokem@gmail.com>
2024-08-11 16:58:36 +0100

    still on the responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "530px",
+        smxl: "765px",
+        md: "960px",
+        smlg: "1110px",
+        lg: "1230px",
+        xllg: "1350px",
+      },
+    },
   },
   plugins: [],
 };
